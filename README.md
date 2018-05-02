7c36d6e537da505eb59507e5c86163b607f24d46
Pedro H.  G. Souto <phgsouto@gmail.com>
2018-05-02 20:01:09 -0300

    Initial commit
    Basic functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+## Uptime.exe
+
+A simple uptime utility to show the system uptime, since Windows does not have this command nativelly.
+Written in C# .Net.
+
+Version = 0.1
+
+Enjoy!
--- Uptime/Program.cs
+++ Uptime/Program.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace Uptime {
     class Program {
         static void Main(string[] args) {
+
+            // Get system hostname
+            string hostname = System.Environment.MachineName;
+
+            // Get system uptime
+            PerformanceCounter upTime = new PerformanceCounter("System", "System Up Time");
+            upTime.NextValue(); // get value one time to avoid zero on load
+            string fmt = "0#"; // number format
+            TimeSpan ts = TimeSpan.FromSeconds(upTime.NextValue());
+            string sUptime = ts.Days + "d " + ts.Hours.ToString(fmt)
+                + ":" + ts.Minutes.ToString(fmt)
+                + ":" + ts.Seconds.ToString(fmt);
+
+            Console.WriteLine(hostname + " is up for " + sUptime);
+
         }
     }
 }
