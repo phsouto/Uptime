ade464ae236032b50290451f951e412d311451c2
Pedro H.  G. Souto <phgsouto@gmail.com>
2018-06-01 09:33:16 -0300

    Version 0.1.007, adding '--version' option

--- Uptime/Program.cs
+++ Uptime/Program.cs
@@ -8,20 +8,31 @@ namespace Uptime {
     class Program {
         static void Main(string[] args) {
 
-            // Get system hostname
-            string hostname = System.Environment.MachineName;
+            if(args.Length == 0) {
+                // Get system hostname
+                string hostname = System.Environment.MachineName;
 
-            // Get system uptime
-            PerformanceCounter upTime = new PerformanceCounter("System", "System Up Time");
-            upTime.NextValue(); // get value one time to avoid zero on load
-            string fmt = "0#"; // number format
-            TimeSpan ts = TimeSpan.FromSeconds(upTime.NextValue());
-            string sUptime = ts.Days + "d " + ts.Hours.ToString(fmt)
-                + ":" + ts.Minutes.ToString(fmt)
-                + ":" + ts.Seconds.ToString(fmt);
+                // Get system uptime
+                PerformanceCounter upTime = new PerformanceCounter("System", "System Up Time");
+                upTime.NextValue(); // get value one time to avoid zero on load
+                string fmt = "0#"; // number format
+                TimeSpan ts = TimeSpan.FromSeconds(upTime.NextValue());
+                string sUptime = ts.Days + "d " + ts.Hours.ToString(fmt)
+                    + ":" + ts.Minutes.ToString(fmt)
+                    + ":" + ts.Seconds.ToString(fmt);
 
-            Console.WriteLine(hostname + " is up for " + sUptime);
+                Console.WriteLine(hostname + " is up for " + sUptime);
+            } else {
+                if (args[0] == "--version") {
+                    string maj_ver = "0";
+                    string min_ver = "1";
+                    string build = "007";
+                    string build_date = "2018.06.01";
 
+                    Console.WriteLine("Uptime version {0}.{1} build {2} of {3}", maj_ver, min_ver, build, build_date);
+                    Console.WriteLine("By phsouto : https://github.com/phsouto");
+                }
+            }
         }
     }
 }
